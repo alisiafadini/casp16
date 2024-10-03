1783c8ce5a6e0e1afa14064aecc1966257a2d16a
randyjread <rjr27@cam.ac.uk>
2024-10-03 15:32:31 +0100

    Update CAPRI website URL

--- qmode3/scripts/score_massivefold_functions.py
+++ qmode3/scripts/score_massivefold_functions.py
@@ -146,7 +146,7 @@ def process_target(target_id_og, targetid_foroutput, ref_file, num_proc, scoring
     output_id = targetid_foroutput.split('.')[0].strip()
     
     # Step 2: Download the corresponding massivefold target dataset
-    download_url = f"https://casp-capri.sinbios.plbs.fr/index.php/s/TTqScLKZM5W6ZFi/download?path=%2Fgathered_runs&files={target_id_og.strip()}_MassiveFold_all_pdbs.tar.gz"
+    download_url = f"https://casp-capri.sinbios.plbs.fr/index.php/s/zkbP65Bz6SM3Xed/download?path=%2Fgathered_runs&files={target_id_og.strip()}_MassiveFold_all_pdbs.tar.gz"
     output_filename = f"{target_id}_MassiveFold_all_pdbs.tar.gz"
     subprocess.run(['wget', download_url, '-O', output_filename])
 
